05c3717e875236f89c6a82cf8a30bb84c84632b2
satitpongjansawang <satitpongjansawang@gmail.com>
2021-12-12 22:00:03 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-liff
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-liff)
---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./style.css";
+import {useState , useEffect} from 'react';
+import liff from '@line/liff';
+
+ function App() {
+  const [pictureUrl, setPictureUrl] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [userId, setUserId] = useState('');
+  const [email , setEmail] = useState('');
+  
+
+const initLine = () => {
+  liff.init({ liffId : "1655945045-NpP9E2aG"}, () => {
+    if (liff.isLoggedIn() ){
+      runApp();
+    }else{
+      liff.login();
+    }
+  }, err => console.error(err));
+}
+
+const runApp = () =>{
+  liff.getProfile().then(profile => {
+    setDisplayName(profile.displayName);
+    setPictureUrl(profile.pictureUrl);
+    setUserId(profile.userId);
+    setEmail(liff.getDecodedIDToken().email);
+  }).catch(err => console.error(err));
+}
+
+useEffect(() => {
+  initLine();
+},  []);
+return(
+  <div className="App">
+  <img className="profile" src={pictureUrl} />
+  <p>{displayName}</p>
+  <p>{email}</p>
+    <div class="card text-center">
+      <div class="card-header">
+        Featured
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+      <div class="card-footer text-muted">
+        2 days ago
+      </div>
+    </div>
+  </div>
+);
+
+
+ }
+
+ export default App;
