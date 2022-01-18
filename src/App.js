5006c4f6fb95f3bb2b1f7dee93dbf65969866d5c
satitpongjansawang <satitpongjansawang@gmail.com>
2022-01-18 11:32:48 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -11,11 +11,13 @@ function App() {//LINE_UserID	pictureUrl	displayname	os	language	email	phone
   const [lang, setLang] = useState('');
   const [email , setEmail] = useState('');
   const [phone, setPhone] = useState('');
+  const [carplate , setCarPlate] = useState('');// number of car license plate
 
   const [sel1, setSel1] = useState('');
   const [sel2, setSel2] = useState('');
   const [sel3, setSel3] = useState('');
   const [sel4, setSel4] = useState('');
+  const [sel5, setSel5] = useState('');// province of car license plate
   const [notes, setNotes] = useState('');
   const [appoidate , setAppoiDate] = useState('');
 
@@ -45,7 +47,7 @@ function App() {//LINE_UserID	pictureUrl	displayname	os	language	email	phone
   },  []);
 
   function postData(){
-    let data = {userId , pictureUrl , displayName ,os , lang ,email , phone , sel1 , sel2 , sel3 , sel4, notes , appoidate}
+    let data = {userId , pictureUrl , displayName ,os , lang ,email , phone , carplate , sel1 , sel2 , sel3 , sel4 , sel5 , notes , appoidate}
     fetch("https://script.google.com/macros/s/AKfycbyyTEHQ_uZhQQwFwy7k9mFkWkUWlJ7KQlbdvRf5mMDXpcnxtWci/exec",{
       method: "POST",
       body: JSON.stringify(data)
@@ -84,6 +86,103 @@ function App() {//LINE_UserID	pictureUrl	displayname	os	language	email	phone
               <option value="mx-5">MX-5</option>
             </select>
           </div>
+
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon5">เลขทะเบียนรถ</span>
+            </div>
+            <input type="text" value={carplate} class="form-control" placeholder="1กท1234" aria-describedby="basic-addon5" onChange={(e) =>{setCarPlate(e.target.value)} }/>
+          </div>   
+
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="inputGroupSelect05">จังหวัดของทะเบียนรถ</label>
+            </div>
+            <select class="custom-select" id="inputGroupSelect05" value={sel5} onChange={e=>setSel5(e.target.value)}>
+              <option selected disabled value="">เลือก...</option>
+              <option value="กรุงเทพมหานคร">กรุงเทพมหานคร</option>
+              <option value="กระบี่">กระบี่ </option>
+              <option value="กาญจนบุรี">กาญจนบุรี </option>
+              <option value="กาฬสินธุ์">กาฬสินธุ์ </option>
+              <option value="กำแพงเพชร">กำแพงเพชร </option>
+              <option value="ขอนแก่น">ขอนแก่น</option>
+              <option value="จันทบุรี">จันทบุรี</option>
+              <option value="ฉะเชิงเทรา">ฉะเชิงเทรา </option>
+              <option value="ชัยนาท">ชัยนาท </option>
+              <option value="ชัยภูมิ">ชัยภูมิ </option>
+              <option value="ชุมพร">ชุมพร </option>
+              <option value="ชลบุรี">ชลบุรี </option>
+              <option value="เชียงใหม่">เชียงใหม่ </option>
+              <option value="เชียงราย">เชียงราย </option>
+              <option value="ตรัง">ตรัง </option>
+              <option value="ตราด">ตราด </option>
+              <option value="ตาก">ตาก </option>
+              <option value="นครนายก">นครนายก </option>
+              <option value="นครปฐม">นครปฐม </option>
+              <option value="นครพนม">นครพนม </option>
+              <option value="นครราชสีมา">นครราชสีมา </option>
+              <option value="นครศรีธรรมราช">นครศรีธรรมราช </option>
+              <option value="นครสวรรค์">นครสวรรค์ </option>
+              <option value="นราธิวาส">นราธิวาส </option>
+              <option value="น่าน">น่าน </option>
+              <option value="นนทบุรี">นนทบุรี </option>
+              <option value="บึงกาฬ">บึงกาฬ</option>
+              <option value="บุรีรัมย์">บุรีรัมย์</option>
+              <option value="เบตง">เบตง</option>
+              <option value="ประจวบคีรีขันธ์">ประจวบคีรีขันธ์ </option>
+              <option value="ปทุมธานี">ปทุมธานี </option>
+              <option value="ปราจีนบุรี">ปราจีนบุรี </option>
+              <option value="ปัตตานี">ปัตตานี </option>
+              <option value="พะเยา">พะเยา </option>
+              <option value="พระนครศรีอยุธยา">พระนครศรีอยุธยา </option>
+              <option value="พังงา">พังงา </option>
+              <option value="พิจิตร">พิจิตร </option>
+              <option value="พิษณุโลก">พิษณุโลก </option>
+              <option value="เพชรบุรี">เพชรบุรี </option>
+              <option value="เพชรบูรณ์">เพชรบูรณ์ </option>
+              <option value="แพร่">แพร่ </option>
+              <option value="พัทลุง">พัทลุง </option>
+              <option value="ภูเก็ต">ภูเก็ต </option>
+              <option value="มหาสารคาม">มหาสารคาม </option>
+              <option value="มุกดาหาร">มุกดาหาร </option>
+              <option value="แม่ฮ่องสอน">แม่ฮ่องสอน </option>
+              <option value="ยโสธร">ยโสธร </option>
+              <option value="ยะลา">ยะลา </option>
+              <option value="ร้อยเอ็ด">ร้อยเอ็ด </option>
+              <option value="ระนอง">ระนอง </option>
+              <option value="ระยอง">ระยอง </option>
+              <option value="ราชบุรี">ราชบุรี</option>
+              <option value="ลพบุรี">ลพบุรี </option>
+              <option value="ลำปาง">ลำปาง </option>
+              <option value="ลำพูน">ลำพูน </option>
+              <option value="เลย">เลย </option>
+              <option value="ศรีสะเกษ">ศรีสะเกษ</option>
+              <option value="สกลนคร">สกลนคร</option>
+              <option value="สงขลา">สงขลา </option>
+              <option value="สมุทรสาคร">สมุทรสาคร </option>
+              <option value="สมุทรปราการ">สมุทรปราการ </option>
+              <option value="สมุทรสงคราม">สมุทรสงคราม </option>
+              <option value="สระแก้ว">สระแก้ว </option>
+              <option value="สระบุรี">สระบุรี </option>
+              <option value="สิงห์บุรี">สิงห์บุรี </option>
+              <option value="สุโขทัย">สุโขทัย </option>
+              <option value="สุพรรณบุรี">สุพรรณบุรี </option>
+              <option value="สุราษฎร์ธานี">สุราษฎร์ธานี </option>
+              <option value="สุรินทร์">สุรินทร์ </option>
+              <option value="สตูล">สตูล </option>
+              <option value="หนองคาย">หนองคาย </option>
+              <option value="หนองบัวลำภู">หนองบัวลำภู </option>
+              <option value="อำนาจเจริญ">อำนาจเจริญ </option>
+              <option value="อุดรธานี">อุดรธานี </option>
+              <option value="อุตรดิตถ์">อุตรดิตถ์ </option>
+              <option value="อุทัยธานี">อุทัยธานี </option>
+              <option value="อุบลราชธานี">อุบลราชธานี</option>
+              <option value="อ่างทอง">อ่างทอง </option>
+            </select>
+          </div>
+
+
+
 
           <div class="input-group mb-3">
             <div class="input-group-prepend">
